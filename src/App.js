6184a8367198dc720c689e7513b451f8b2474aec
Oreda-Z <shtucer31@gmail.com>
2020-08-22 10:12:25 +0300

    lesson 11

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://s2.logaster.com/static/v3/img/products/logo.png"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Dialogs</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Setting</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div className="img-header">
+                    <img src="https://interier-foto.ru/wp-content/uploads/dlinnye-foto-4.jpg"/>
+                </div>
+                <div>
+                    <img src='https://ramonllullenglish4all.files.wordpress.com/2010/02/african_man.jpg'/>
+                </div>
+                <div>
+                    ava + descritpion
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                        <div>post 1</div>
+                        <div>post 2</div>
+                        <div>post 3</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
