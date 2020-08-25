4a1e4897fa20342e174fbd0bf3eb51a59b7b42f9
Oreda-Z <shtucer31@gmail.com>
2020-08-25 08:46:07 +0300

    lesson 19 (route, browser-router)

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter} from "react-router-dom";
+import Route from "react-router-dom/es/Route";
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
-            <Navbar />
+            <Navbar/>
             <div className="app-wrapper-content">
-                {/*<Profile />*/}
-                <Dialogs />
+                <Route path='/profile' component={Profile}/>
+                <Route path='/messages' component={Dialogs}/>
             </div>
-
         </div>
+        </BrowserRouter>
     );
 }
 
