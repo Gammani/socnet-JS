bd141a4cc25aeee405cf1f46a8878c33cea20dbf
Oreda-Z <shtucer31@gmail.com>
2021-03-05 07:15:48 +0300

    lesson 49 (практика - страница пользователей)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Setting from "./components/Setting/Setting";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
+
 
 const App = (props) => {
     return (
@@ -18,7 +19,7 @@ const App = (props) => {
             <div className="app-wrapper-content">
                 <Route path='/profile' render={() => <Profile/>}/>
                 <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                <Route path='/users' render={() => <Users/>}/>
+                <Route path='/users' render={() => <UsersContainer />}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
                 <Route path='/setting' render={() => <Setting/>}/>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,82 @@
 import React from "react";
+import styles from './users.module.css'
 
 let Users = (props) => {
+
+    if (props.users.length === 0) {
+        props.setUsers([
+            {
+                id: 1,
+                photoUrl: 'https://avatarko.ru/img/kartinka/1/multfilm_gomer.png',
+                followed: true,
+                fullName: "Leha",
+                status: "Yo",
+                location: {city: "Devyatkino", country: "Russia"}
+            },
+            {
+                id: 2,
+                photoUrl: 'https://avatarko.ru/img/kartinka/1/multfilm_gomer.png',
+                followed: true,
+                fullName: "Suzi",
+                status: "I'm fine",
+                location: {city: "New York", country: "USA"}
+            },
+            {
+                id: 3,
+                photoUrl: 'https://avatarko.ru/img/kartinka/1/multfilm_gomer.png',
+                followed: true,
+                fullName: "Fai",
+                status: "Hi hi",
+                location: {city: "Pattaya", country: "Thailand"}
+            },
+            {
+                id: 4,
+                photoUrl: 'https://avatarko.ru/img/kartinka/1/multfilm_gomer.png',
+                followed: true,
+                fullName: "Daenerys",
+                status: "Where is my Dragons?",
+                location: {city: "Royal Harbor", country: "Seven Kingdoms"}
+            },
+            {
+                id: 5,
+                photoUrl: 'https://avatarko.ru/img/kartinka/1/multfilm_gomer.png',
+                followed: false,
+                fullName: "name",
+                status: "online",
+                location: {city: "city", country: "country"}
+            }
+        ])
+    }
+
     return (
         <div>
-            USERS WILL BE HERE
+            {
+                props.users.map(u => <div key={u.id}>
+                    <span>
+                        <div>
+                            <img src={u.photoUrl} className={styles.userPhoto} alt={"аватар"}/>
+                        </div>
+                        <div>
+                           {u.followed ?
+                               <button onClick={() => {
+                                   props.unFollow(u.id)
+                               }}>UnFollow</button> :
+                               <button onClick={() => {
+                                   props.follow(u.id)
+                               }}>Follow</button>}
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{u.fullName}</div><div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{u.location.country}</div>
+                            <div>{u.location.city}</div>
+                        </span>
+                    </span>
+                </div>)
+            }
         </div>
     )
 }
