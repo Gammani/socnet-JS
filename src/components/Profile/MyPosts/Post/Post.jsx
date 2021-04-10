74a148e84ec8843c7fee68b81822c3f8e4639073
Oreda-Z <shtucer31@gmail.com>
2021-04-10 14:39:17 +0300

    lesson 75 redux-form введение (login)

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,10 +7,10 @@ const Post = (props) => {
             <div>
                 <div className={s.ava}>
                     <img
-                        src='https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'/>
+                        src='https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' alt={"avatar"}/>
                     {props.message}
                     <div className={s.like}>
-                        <img src='https://banner2.cleanpng.com/20181001/rhs/kisspng-like-button-social-media-image-facebook-messenger-health-insurance-life-insurance-and-medicare-plan-5bb209e2eacd98.4287911215383945949618.jpg' /> {props.likesCount}
+                        <img src='https://banner2.cleanpng.com/20181001/rhs/kisspng-like-button-social-media-image-facebook-messenger-health-insurance-life-insurance-and-medicare-plan-5bb209e2eacd98.4287911215383945949618.jpg' alt={"avatar"}/> {props.likesCount}
                     </div>
                 </div>
             </div>
